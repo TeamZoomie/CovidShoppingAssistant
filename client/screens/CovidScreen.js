2b2cd72d8eacff3455be507e5f8439fb45596227
Theo <education35879871023@gmail.com>
2020-08-31 16:09:10 +1000

    Added display of data, along with a template to customise colours and other configurations

--- client/screens/CovidScreen.js
+++ client/screens/CovidScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Dimensions } from 'react-native';
 import { Icon, TopNavigation, TopNavigationAction} from '@ui-kitten/components';
 import { List, ListItem, Divider } from '@ui-kitten/components';
 import { Datepicker, Layout, Text } from '@ui-kitten/components';
+import { LineChart } from "react-native-chart-kit";
 
 const styles = StyleSheet.create({
     root: {
@@ -12,6 +13,12 @@ const styles = StyleSheet.create({
         maxHeight: 200,
     }
 })
+
+const chartConfig = {
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    backgroundGradientFrom: "#FFFFFF",
+    backgroundGradientTo: "#FFFFFF",
+  };
 
 const HamburgerIcon = (props) => (
     <Icon {...props} name='menu-outline' />
@@ -26,27 +33,30 @@ const data_copy = {"country":"Australia","cases":24916,"todayCases":104,"deaths"
 
 // In QLD, from the 13th of March to the 31st of August
 // https://www.covid19data.com.au/states-and-territories
-const case_numbers_historical = [8, 11, 15, 7, 10, 16, 50, 40, 37, 38, 60, 78,
+const case_numbers_historical = {
+    labels: [Array(172).keys()],
+    datasets: [
+        {
+    data:  [8, 11, 15, 7, 10, 16, 50, 40, 37, 38, 60, 78,
     46, 50, 62, 70, 31, 33, 55, 40, 57, 39, 27, 9, 14, 13, 9, 10, 13, 9, 12, 7,
     11, 5, 5, 6, 9, 5, 0, 6, 0, 2, 2, 2, 3, 3, 0, 1, 0, 0, 1, 1, 3, 5, 0, 2, 0,
     0, 2, 0, 0, 0, 0, 1, 1, 0, 2, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1,
     0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1,
     0, 2, 0, 0, 0, 0, 3, 3, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 
-    0, 0, 1, 1, 0, 9, 2, 1, 0, 1, 2, 3, 4, 4, 1
-]
+    0, 0, 1, 1, 0, 9, 2, 1, 0, 1, 2, 3, 4, 4, 1]
+    }
+]}
 
 export const CovidScreen = ({ navigation }) => {
 
-    const renderItem = ({ item }) => (
-        <ListItem title={`${item}`}/>
-    );
-
-    const DrawerAction = () => (
-        <TopNavigationAction icon={HamburgerIcon} onPress={() => navigation.openDrawer()}/>
-    );
-
     const [date, setDate] = React.useState(new Date());
+
+    // Define the drawer action
+    const DrawerAction = () => (
+        <TopNavigationAction icon={HamburgerIcon} onPress={() => 
+            navigation.openDrawer()}/>
+    );
 
     return(
         <SafeAreaView style={styles.root}>
@@ -55,12 +65,6 @@ export const CovidScreen = ({ navigation }) => {
                     title='COVID-19 Information' 
                     alignment='center' 
                     accessoryLeft={DrawerAction}
-                />
-                <List
-                    style={styles.container}
-                    data={data_copy}
-                    ItemSeparatorComponent={Divider}
-                    renderItem={renderItem}
                 />
             </View>
             <Layout>
@@ -71,6 +75,20 @@ export const CovidScreen = ({ navigation }) => {
                 <Datepicker
                     date={date}
                     onSelect={nextDate => setDate(nextDate)}
+                />
+
+                <Divider/>
+                <Text category='h6'>
+                    Number of New COVID-19 Cases Per Day
+                </Text>
+                <LineChart
+                    data={case_numbers_historical}
+                    width={Dimensions.get('window').width}
+                    height={220}
+                    chartConfig={chartConfig}
+                    withVerticalLabels={false}
+                    withHorizontalLines={false}
+                    withVerticalLines={false}
                 />
             </Layout>
         </SafeAreaView>
