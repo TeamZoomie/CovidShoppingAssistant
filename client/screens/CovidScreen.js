d656023863cd25e95459785c713b0865c18e4b04
AndyJiePeng <andyjie18pen@gmail.com>
2020-08-31 15:13:56 +0800

    Retrive the covid19 stas from api, still help to display it on the web

--- client/screens/CovidScreen.js
+++ client/screens/CovidScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet, View, Dimensions } from 'react-native';
 import { Icon, TopNavigation, TopNavigationAction} from '@ui-kitten/components';
 import { List, ListItem, Divider } from '@ui-kitten/components';
@@ -26,8 +26,10 @@ const HamburgerIcon = (props) => (
 
 // https://github.com/javieraviles/covidAPI
 const data = 'https://coronavirus-19-api.herokuapp.com/countries/Australia'
-
+var covidInfo
 // TODO Need to read data in directly
+
+
 
 const data_copy = {"country":"Australia","cases":24916,"todayCases":104,"deaths":517,"todayDeaths":15,"recovered":19603,"active":4796,"critical":38,"casesPerOneMillion":975,"deathsPerOneMillion":20,"totalTests":5757173,"testsPerOneMillion":225388}
 
@@ -58,6 +60,23 @@ export const CovidScreen = ({ navigation }) => {
             navigation.openDrawer()}/>
     );
 
+    // Retrive the Covid19 stats
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = (e) => {
+    if (request.readyState !== 4) {
+        return;
+    }
+
+    if (request.status === 200) {
+        console.log('success', request.responseText);
+        covidInfo = request.responseText;
+        console.log(covidInfo);
+    } else {
+        console.warn('error');
+    }
+    };
+    request.open('GET', 'https://coronavirus-19-api.herokuapp.com/countries/Australia');
+    request.send();
     return(
         <SafeAreaView style={styles.root}>
             <View>
@@ -93,6 +112,13 @@ export const CovidScreen = ({ navigation }) => {
                     withHorizontalLines={false}
                     withVerticalLines={false}
                 />
+                <Divider/>
+                <Text category='h6'>
+                    Number of New COVID-19 Cases - {date.toLocaleDateString()}
+                </Text> 
+                <Text>
+                    Stats: {covidInfo}
+                </Text>
             </Layout>
         </SafeAreaView>
     );
