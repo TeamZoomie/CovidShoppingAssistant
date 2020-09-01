4c141760d02beab16c8cb85b08dd344305afc377
Henchman777 <calumhenman7@gmail.com>
2020-09-01 22:11:01 +0000

    Added another test app

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('api.urls')),
+    path('quickstart/', include('quickstart.urls')),
     path('admin/', admin.site.urls),
 ]
