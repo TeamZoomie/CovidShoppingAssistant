0bb5da6db5af4640e07b6c3306ccc748e2f74de4
Henchman777 <calumhenman7@gmail.com>
2020-09-03 11:31:59 +0000

    Covid stats page only returns the latest Covid data when refreshed

--- server/covidstats/views.py
+++ server/covidstats/views.py
@@ -10,24 +10,21 @@ class CovidViewSet(viewsets.ModelViewSet):
     """
     API endpoint
     """
-    data = requests.get('https://coronavirus-19-api.herokuapp.com/countries/Australia')
-
-    instance = CovidAustralia.objects.create(country = data.json()['country'],
-            cases = data.json()['cases'],
-            todayCases = data.json()['todayCases'],
-            deaths = data.json()['deaths'],
-            todayDeaths = data.json()['todayDeaths'],
-            recovered = data.json()['recovered'],
-            active = data.json()['active'],
-            critical = data.json()['critical'],
-            casesPerOneMillion = data.json()['casesPerOneMillion'],
-            deathsPerOneMillion = data.json()['deathsPerOneMillion'],
-            totalTests = data.json()['totalTests'],
-            testsPerOneMillion = data.json()['testsPerOneMillion'])
-
-    
-    
     queryset = CovidAustralia.objects.all()
     serializer_class = CovidSerializer
 
-    
+    CovidAustralia.objects.all().delete()
+
+data = requests.get('https://coronavirus-19-api.herokuapp.com/countries/Australia')
+instance = CovidAustralia.objects.create(country = data.json()['country'],
+                cases = data.json()['cases'],
+                todayCases = data.json()['todayCases'],
+                deaths = data.json()['deaths'],
+                todayDeaths = data.json()['todayDeaths'],
+                recovered = data.json()['recovered'],
+                active = data.json()['active'],
+                critical = data.json()['critical'],
+                casesPerOneMillion = data.json()['casesPerOneMillion'],
+                deathsPerOneMillion = data.json()['deathsPerOneMillion'],
+                totalTests = data.json()['totalTests'],
+                testsPerOneMillion = data.json()['testsPerOneMillion'])
