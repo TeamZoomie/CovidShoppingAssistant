3176b698df8815128a15a0eb7adb9f3bd5da47af
Henchman777 <calumhenman7@gmail.com>
2020-09-03 12:01:59 +0000

    It still wasn't working so I used a different method to write the code. If it exists in the database, update it, otherwise create it

--- server/covidstats/views.py
+++ server/covidstats/views.py
@@ -16,15 +16,23 @@ class CovidViewSet(viewsets.ModelViewSet):
     CovidAustralia.objects.all().delete()
 
 data = requests.get('https://coronavirus-19-api.herokuapp.com/countries/Australia')
-instance = CovidAustralia.objects.create(country = data.json()['country'],
-                cases = data.json()['cases'],
-                todayCases = data.json()['todayCases'],
-                deaths = data.json()['deaths'],
-                todayDeaths = data.json()['todayDeaths'],
-                recovered = data.json()['recovered'],
-                active = data.json()['active'],
-                critical = data.json()['critical'],
-                casesPerOneMillion = data.json()['casesPerOneMillion'],
-                deathsPerOneMillion = data.json()['deathsPerOneMillion'],
-                totalTests = data.json()['totalTests'],
-                testsPerOneMillion = data.json()['testsPerOneMillion'])
+update = {'country': data.json()['country'], 
+            'cases': data.json()['cases'],                      
+            'todayCases': data.json()['todayCases'],            
+            'deaths': data.json()['deaths'],                    
+            'todayDeaths': data.json()['todayDeaths'],          
+            'recovered': data.json()['recovered'],              
+            'active': data.json()['active'],                    
+            'critical': data.json()['critical'],                
+            'casesPerOneMillion': data.json()['casesPerOneMillion'],
+            'deathsPerOneMillion': data.json()['deathsPerOneMillion'],                                                                'totalTests': data.json()['totalTests'],            
+            'testsPerOneMillion': data.json()['testsPerOneMillion']}
+        
+try:                                                               
+    obj = CovidAustralia.objects.get(country='Australia')          
+    for key, value in update.items():                              
+        setattr(obj, key, value)                                   
+        obj.save()                                                     
+except CovidAustralia.DoesNotExist:                                
+    obj = CovidAustralia(**update)
+    obj.save()
