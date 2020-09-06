fa748cbcd2d0712db5f053c1b31537243ea76990
Henchman <calumhenman7@gmail.com>
2020-09-06 20:23:50 +1000

    Adding news files one at a time

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j$7mrlmw*(jaxe82_sep4&30*j^yk&g#goxr(*706jd6ap)9$r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deco3801-zoomie.uqcloud.net']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'covidstats.apps.CovidstatsConfig',
+    'rest_framework',
+    'requests',
+    'bs4',
+    'feedparser'
 ]
 
 MIDDLEWARE = [
--- api/covidstats/views.py
+++ api/covidstats/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import CovidSerializer
+from .models import CovidAustralia
+import requests
+
+
+class CovidViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for gathering latest covid data
+    """
+    queryset = CovidAustralia.objects.all()
+    serializer_class = CovidSerializer
+
+    CovidAustralia.objects.all().delete()
+
+
+data = requests.get('https://coronavirus-19-api.herokuapp.com/countries/Australia')
+update = {'country': data.json()['country'],
+          'cases': data.json()['cases'],
+          'todayCases': data.json()['todayCases'],
+          'deaths': data.json()['deaths'],
+          'todayDeaths': data.json()['todayDeaths'],
+          'recovered': data.json()['recovered'],
+          'active': data.json()['active'],
+          'critical': data.json()['critical'],
+          'casesPerOneMillion': data.json()['casesPerOneMillion'],
+          'deathsPerOneMillion': data.json()['deathsPerOneMillion'], 'totalTests': data.json()['totalTests'],
+          'testsPerOneMillion': data.json()['testsPerOneMillion']}
+
+try:
+    obj = CovidAustralia.objects.get(country='Australia')
+    for key, value in update.items():
+        setattr(obj, key, value)
+        obj.save()
+except CovidAustralia.DoesNotExist:
+    obj = CovidAustralia(**update)
+    obj.save()
