ed6d15f4a084345eaa5bcaa68e7313a69cc6d8dc
Henchman <calumhenman7@gmail.com>
2020-09-10 10:05:02 +1000

    deleted:    server/Server-setup.md 	deleted:    server/api/__init__.py 	deleted:    server/api/__pycache__/__init__.cpython-36.pyc 	deleted:    server/api/__pycache__/__init__.cpython-38.pyc 	deleted:    server/api/__pycache__/admin.cpython-36.pyc 	deleted:    server/api/__pycache__/apps.cpython-36.pyc 	deleted:    server/api/__pycache__/apps.cpython-38.pyc 	deleted:    server/api/__pycache__/models.cpython-36.pyc 	deleted:    server/api/__pycache__/serializers.cpython-36.pyc 	deleted:    server/api/__pycache__/urls.cpython-36.pyc 	deleted:    server/api/__pycache__/urls.cpython-38.pyc 	deleted:    server/api/__pycache__/views.cpython-36.pyc 	deleted:    server/api/admin.py 	deleted:    server/api/apps.py 	deleted:    server/api/migrations/0001_initial.py 	deleted:    server/api/migrations/__init__.py 	deleted:    server/api/migrations/__pycache__/0001_initial.cpython-36.pyc 	deleted:    server/api/migrations/__pycache__/__init__.cpython-36.pyc 	deleted:    server/api/models.py 	deleted:    server/api/serializers.py 	deleted:    server/api/tests.py 	deleted:    server/api/urls.py 	deleted:    server/api/views.py 	deleted:    server/covidstats/__init__.py 	deleted:    server/covidstats/__pycache__/__init__.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/admin.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/apps.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/models.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/serializers.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/urls.cpython-36.pyc 	deleted:    server/covidstats/__pycache__/views.cpython-36.pyc 	deleted:    server/covidstats/admin.py 	deleted:    server/covidstats/apps.py 	deleted:    server/covidstats/migrations/0001_initial.py 	deleted:    server/covidstats/migrations/__init__.py 	deleted:    server/covidstats/migrations/__pycache__/0001_initial.cpython-36.pyc 	deleted:    server/covidstats/migrations/__pycache__/__init__.cpython-36.pyc 	deleted:    server/covidstats/models.py 	deleted:    server/covidstats/serializers.py 	deleted:    server/covidstats/tests.py 	deleted:    server/covidstats/urls.py 	deleted:    server/covidstats/views.py 	deleted:    server/covidstats/views.py.bak 	deleted:    server/db.sqlite3 	deleted:    server/manage.py 	deleted:    server/quickstart/__init__.py 	deleted:    server/quickstart/__pycache__/__init__.cpython-36.pyc 	deleted:    server/quickstart/__pycache__/serializers.cpython-36.pyc 	deleted:    server/quickstart/__pycache__/urls.cpython-36.pyc 	deleted:    server/quickstart/__pycache__/views.cpython-36.pyc 	deleted:    server/quickstart/admin.py 	deleted:    server/quickstart/apps.py 	deleted:    server/quickstart/migrations/__init__.py 	deleted:    server/quickstart/models.py 	deleted:    server/quickstart/serializers.py 	deleted:    server/quickstart/tests.py 	deleted:    server/quickstart/urls.py 	deleted:    server/quickstart/views.py 	deleted:    server/script/article.py 	deleted:    server/script/news.json 	deleted:    server/server/__init__.py 	deleted:    server/server/__pycache__/__init__.cpython-36.pyc 	deleted:    server/server/__pycache__/__init__.cpython-38.pyc 	deleted:    server/server/__pycache__/settings.cpython-36.pyc 	deleted:    server/server/__pycache__/settings.cpython-38.pyc 	deleted:    server/server/__pycache__/urls.cpython-36.pyc 	deleted:    server/server/__pycache__/urls.cpython-38.pyc 	deleted:    server/server/__pycache__/wsgi.cpython-36.pyc 	deleted:    server/server/asgi.py 	deleted:    server/server/settings.py 	deleted:    server/server/urls.py 	deleted:    server/server/wsgi.py

--- server/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Legends
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Legends
-        fields = ('name', 'alias')
