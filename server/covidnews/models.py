7a4b7ac3eabb8f521c2f786343f3182f89474d72
Henchman <calumhenman7@gmail.com>
2020-09-13 13:35:16 +1000

    Updated news to show news from Australia

--- server/covidnews/models.py
+++ server/covidnews/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class CovidNews(models.Model):
-    version = models.CharField(max_length=60)
+    description = models.CharField(max_length = 400, default='desc')
+    publishedDate = models.CharField(max_length = 200, default='date')
+    title = models.CharField(max_length = 200, default='title')
+    url = models.CharField(max_length = 200, default='url')
 
     def __str__(self):
-        return self.version
+        return self.description
