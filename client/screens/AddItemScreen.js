9f536435cd70cfb51f46e449b39697b2918e738b
Theo Duval <education35879871023@gmail.com>
2020-10-12 14:00:21 +1000

    Fixed bug with importing in AddItemScreen

--- client/screens/AddItemScreen.js
+++ client/screens/AddItemScreen.js
@@ -1,6 +1,6 @@
-import { React, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import { ScrollView, StyleSheet, View, Image } from 'react-native';
-import { Heading } from '../components/Heading';
+import Heading from '../components/Heading';
 import { Input, Layout, Text, withStyles } from '@ui-kitten/components';
 
 const styles = (theme) => ({
@@ -10,8 +10,13 @@ const styles = (theme) => ({
     },
     content: {
         flex: 1
+    },
+    text: {
+        fontWeight: "700",
+        textAlign: 'left',
+        marginHorizontal: 8
     }
-})
+});
 
 const AddItemScreen = ({ eva, navigation }) => {
 
@@ -20,7 +25,7 @@ const AddItemScreen = ({ eva, navigation }) => {
     return (
         <View style={styles.root}>
             <Layout style={styles.content}>
-                <Text>Test</Text>
+                <Heading category="h2" style={{ fontWeight: "700", padding: 16 }}>Test</Heading>
             </Layout>
         </View>
     );
