97bf4e025977dc262d670857fe3a7cd0276fcb2d
Theo Duval <education35879871023@gmail.com>
2020-10-12 17:40:02 +1000

    Added some more options, very basic layout for now

--- client/screens/AddItemScreen.js
+++ client/screens/AddItemScreen.js
@@ -24,10 +24,13 @@ const AddItemScreen = ({ eva, navigation }) => {
     const headingCat = "h6";
 
     // Product Name
-    const [productName, setProductName] = React.useState('');
+    const [productName, setProductName] = React.useState();
 
     // Barcode
-    const [barcode, setBarcode] = React.useState(0);
+    const [barcode, setBarcode] = React.useState();
+
+    // Category - int as defined in ../store-maps/encoding.txt
+    const [category, setCategory] = React.useState();
 
     // Is this product food? Useful for nutrition info
     const [isFood, setIsFood] = React.useState(false);
@@ -35,12 +38,39 @@ const AddItemScreen = ({ eva, navigation }) => {
     // Energy
     const [energy, setEnergy] = React.useState();
 
+    // Protein
+    const [protein, setProtein] = React.useState();
+
+    // Fat
+    const [fat, setFat] = React.useState();
+
+    // Carbs
+    const [carbs, setCarbs] = React.useState();
+
+    // Fibre
+    const [fibre, setFibre] = React.useState();
+
+    // Sodium
+    const [sodium, setSodium] = React.useState();
+
+    // Vegetarian
+    const [vegetarian, setVegetarian] = React.useState();
+
+    // Vegan
+    const [vegan, setVegan] = React.useState();
+
+    // Ingredients TODO Need to make a good way of doing this one as well
+    const [ingerdients, setIngredients] = React.useState();
+
+
+
     return (
         <View style={styles.root}>
             <Layout style={styles.content}>
                 <Heading 
                     category={headingCat} 
-                    style={{ fontWeight: "700", padding: 16 }}>
+                    style={{ fontWeight: "700", padding: 16 }}
+                >
                         Product Name
                 </Heading>
                 <Input
@@ -62,12 +92,61 @@ const AddItemScreen = ({ eva, navigation }) => {
                 <Input
                     placeholder={`energy`}
                     value={energy}
+                    disabled={!isFood}
                     onChangeText={nextValue => setEnergy(nextValue)}
                 />
+                <Input
+                    placeholder={`protein`}
+                    value={protein}
+                    disabled={!isFood}
+                    onChangeText={nextValue => setProtein(nextValue)}
+                />
+                <Input
+                    placeholder={`fat`}
+                    value={fat}
+                    disabled={!isFood}
+                    onChangeText={nextValue => setFat(nextValue)}
+                />
+                <Input
+                    placeholder={`carbohydrates`}
+                    value={carbs}
+                    disabled={!isFood}
+                    onChangeText={nextValue => setCarbs(nextValue)}
+                />
+                <Input
+                    placeholder={`sodium`}
+                    value={sodium}
+                    disabled={!isFood}
+                    onChangeText={nextValue => setSodium(nextValue)}
+                />
 
-                <Heading category={headingCat} style={{ fontWeight: "700", padding: 16 }}>
-                    Barcode
+                
+                
+                <CheckBox
+                    checked={vegetarian}
+                    onChange={nextChecked => setVegetarian(nextChecked)}>
+                        {`Is this product vegetarian?`}
+                </CheckBox>
+                <CheckBox
+                    checked={vegan}
+                    onChange={nextChecked => setVegan(nextChecked)}>
+                        {`Is this product vegan?`}
+                </CheckBox>
+
+                {/*TODO Definitely need a better way for this one*/}
+                <Input
+                    placeholder={`Choose a category.`}
+                    value={category}
+                    onChangeText={nextValue => setCategory(nextValue)}    
+                />
+
+                <Heading 
+                    category={headingCat} 
+                    style={{ fontWeight: "700", padding: 16 }}
+                >
+                        Barcode
                 </Heading>
+
                 <Input
                     placeholder={barcode}
                     value={barcode}
