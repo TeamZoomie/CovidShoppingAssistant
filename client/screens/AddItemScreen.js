7ae4a974db281074dd638239b8bae10e1b2bc481
Theo Duval <education35879871023@gmail.com>
2020-10-12 15:02:33 +1000

    Started form again, need to change layout to be easier

--- client/screens/AddItemScreen.js
+++ client/screens/AddItemScreen.js
@@ -1,7 +1,7 @@
-import React, {useEffect } from 'react';
-import { ScrollView, StyleSheet, View, Image } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import Heading from '../components/Heading';
-import { Input, Layout, Text, withStyles } from '@ui-kitten/components';
+import { Input, Layout, CheckBox, withStyles } from '@ui-kitten/components';
 
 const styles = (theme) => ({
     root: {
@@ -9,12 +9,10 @@ const styles = (theme) => ({
         backgroundColor: theme['background-basic-color-2']
     },
     content: {
-        flex: 1
-    },
-    text: {
-        fontWeight: "700",
-        textAlign: 'left',
-        marginHorizontal: 8
+        width: '72%',
+        flex: 1,
+        padding: 2,
+        backgroundColor: theme['background-basic-color-2']
     }
 });
 
@@ -22,10 +20,59 @@ const AddItemScreen = ({ eva, navigation }) => {
 
     const styles = eva.style;
 
+    // Keep headings consistent between items
+    const headingCat = "h6";
+
+    // Product Name
+    const [productName, setProductName] = React.useState('');
+
+    // Barcode
+    const [barcode, setBarcode] = React.useState(0);
+
+    // Is this product food? Useful for nutrition info
+    const [isFood, setIsFood] = React.useState(false);
+
+    // Energy
+    const [energy, setEnergy] = React.useState();
+
     return (
         <View style={styles.root}>
             <Layout style={styles.content}>
-                <Heading category="h2" style={{ fontWeight: "700", padding: 16 }}>Test</Heading>
+                <Heading 
+                    category={headingCat} 
+                    style={{ fontWeight: "700", padding: 16 }}>
+                        Product Name
+                </Heading>
+                <Input
+                    placeholder={productName}
+                    value={productName}
+                    onChangeText={nextValue => setProductName(nextValue)}    
+                />
+                
+                <Heading 
+                    category={headingCat} 
+                    style={{ fontWeight: "700", padding: 16 }}>
+                        Nutritional Information
+                </Heading>
+                <CheckBox
+                    checked={isFood}
+                    onChange={nextChecked => setIsFood(nextChecked)}>
+                        {`Is this product food?`}
+                </CheckBox>
+                <Input
+                    placeholder={`energy`}
+                    value={energy}
+                    onChangeText={nextValue => setEnergy(nextValue)}
+                />
+
+                <Heading category={headingCat} style={{ fontWeight: "700", padding: 16 }}>
+                    Barcode
+                </Heading>
+                <Input
+                    placeholder={barcode}
+                    value={barcode}
+                    onChangeText={nextValue => setBarcode(nextValue)}    
+                />
             </Layout>
         </View>
     );
