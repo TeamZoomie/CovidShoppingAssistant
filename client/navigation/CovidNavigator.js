d78f4a0dab21e36724fee955c2d3ab354ba2a7b0
Henchman <calumhenman7@gmail.com>
2020-10-28 20:50:23 +1000

    Addedd comments to the navigation subfolder

--- client/navigation/CovidNavigator.js
+++ client/navigation/CovidNavigator.js
@@ -1,3 +1,7 @@
+/**
+ * Defines the navigation bar which lets users switch between the covid
+ * statistics page and the covid news page.
+ */
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import CovidTabBar from '../screens/CovidScreen/CovidTabBar';
@@ -6,6 +10,9 @@ import StatsScreen from '../screens/CovidScreen/Stats';
 
 const { Navigator, Screen } = createMaterialTopTabNavigator();
 
+/**
+ * The main component.
+ */
 export const CovidNavigator = () => (
     <Navigator tabBar={props => <CovidTabBar {...props}/>}>
       <Screen name='CovidStats' component={StatsScreen}/>
--- client/navigation/index.js
+++ client/navigation/index.js
@@ -1,3 +1,9 @@
+/**
+ * Defines the navigation schema, used in the app to allow users to navigate
+ * around the app. This includes the main navigator that the user sees on the
+ * home screen as well as the covid navigator which allows the user to switch
+ * between covid statistics and news.
+ */
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -10,19 +16,24 @@ import { HomeNavigator } from './HomeNavigator';
 import { CovidNavigator } from './CovidNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
 
-
+// Defines the icon for the lists button
 const ListsIcon = (props) => (
     <Icon {...props} name='list-outline'/>
 );
+// Defines the icon for the news button
 const NewsIcon = (props) => (
     <Icon {...props} name='globe-2-outline'/>
 );
+// Defines the icon for the settings button
 const SettingsIcon = (props) => (
     <Icon {...props} name='options-2-outline'/>
 );
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
+/**
+ * Defines the layout used on the bottom navigator
+ */
 const BottomTabBar = ({ navigation, state }) => (
     <BottomNavigation
         selectedIndex={state.index}
@@ -34,7 +45,7 @@ const BottomTabBar = ({ navigation, state }) => (
     </BottomNavigation>
 );
 
-
+// Defines the container which contains the bottom navigator
 export const AppNavigator = () => (
     <NavigationContainer>
         <Navigator 
